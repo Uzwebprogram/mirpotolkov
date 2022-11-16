7c097f4b57884fcbb84c75250482384b5c39da27
coder <103135051+uzxakker@users.noreply.github.com>
2022-11-16 14:19:49 +0500

    go push

--- src/components/admin/ceilling-option/option-delet-put/index.jsx
+++ src/components/admin/ceilling-option/option-delet-put/index.jsx
@@ -1,54 +1,69 @@
 import { useContext, useEffect, useState } from "react";
 import ModalDelete from "./Modal-delet";
-import { Wrapper} from "./styled-index";
+import { Wrapper } from "./styled-index";
 import ModalPut from "./modal-put";
 import { ChooseContext } from "../../../../context/client/choose/context";
 function OptionDeletPutComponent({}) {
-    const [open , SetOpen] = useState(false)
-    const [open2 , SetOpen2] = useState(false)
-    const [DeletId , setDeleteId] = useState(); 
-    const [PutBlog , setPutBlog] = useState();
-    const [Elements , setElement] = useState();
-    const {ChooseMap} = useContext(ChooseContext);
-    useEffect(() => {
-        ChooseMap.map(elem =>
-            setElement(elem)
-            )
-    }, [Elements])
-    
-    const HandleOpen = (e) => {
-        SetOpen(true)
-        setPutBlog(e.target.value)
-    }
-    const HandleClose = () => {
-        SetOpen(false)
-    }
-    const HandleOpen2 = (e) => {
-        SetOpen2(true)
-        setDeleteId(e.target.value)
-    }
-    const HandleClose2 = () => {
-        SetOpen2(false)
-    }
-     return(
-        <Wrapper>
-                <div >
-                <select onChange={HandleOpen2}>
-                <option  selected disabled> Категория Удалить</option>
-                {ChooseMap.map((elem, index) => (
-                  <option key={index} value={elem.id}>{elem.titleru}</option>
-                ))}
-              </select>
-              <select onChange={HandleOpen}>
-                <option  selected disabled> Категория Изминеть</option>
-                {ChooseMap.map((elem, index) => (
-                  <option key={index}  value={elem.id}>{elem.titleru}</option>
-                ))}
-              </select>
-                </div>
-            <ModalDelete open={open2} HandleClose={HandleClose2}   DeleteId={DeletId}/>
-            <ModalPut open={open} handleClose={HandleClose} Element={Elements} PutBlog={PutBlog} />
-        </Wrapper>
-    )
+  const [open, SetOpen] = useState(false);
+  const [open2, SetOpen2] = useState(false);
+  const [DeletId, setDeleteId] = useState();
+  const [PutBlog, setPutBlog] = useState();
+  const [Elements, setElement] = useState();
+  const { ChooseMap } = useContext(ChooseContext);
+
+  useEffect(() => {
+    ChooseMap.map((elem) => console.log(elem));
+  }, [Elements]);
+  console.log(Elements);
+
+  const HandleOpen = (e) => {
+    SetOpen(true);
+    setPutBlog(e.target.value);
+  };
+  const HandleClose = () => {
+    SetOpen(false);
+  };
+  const HandleOpen2 = (e) => {
+    SetOpen2(true);
+    setDeleteId(e.target.value);
+  };
+  const HandleClose2 = () => {
+    SetOpen2(false);
+  };
+  return (
+    <Wrapper>
+      <div>
+        <select onChange={HandleOpen2}>
+          <option selected disabled>
+            {" "}
+            Категория Удалить
+          </option>
+          {ChooseMap.map((elem, index) => (
+            <option key={index} value={elem.id}>
+              {elem.titleru}
+            </option>
+          ))}
+        </select>
+        <select onChange={HandleOpen}>
+          <option selected disabled>
+            {" "}
+            Категория Изминеть
+          </option>
+          {ChooseMap.map((elem, index) => (
+            <option key={index} value={elem.id}>
+              {elem.titleru}
+            </option>
+          ))}
+        </select>
+      </div>
+      <ModalDelete open={open2} HandleClose={HandleClose2} DeleteId={DeletId} />
+      <ModalPut
+        open={open}
+        handleClose={HandleClose}
+        Element={Elements}
+        PutBlog={PutBlog}
+      />
+    </Wrapper>
+  );
 }
-export default OptionDeletPutComponent
+export default OptionDeletPutComponent;
